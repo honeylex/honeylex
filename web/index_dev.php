f6c3dd33270bdf70a30a4c0ffdf695721cef46f0
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2016-06-01 22:51:46 +0000

    initial commit

---
+++ web/index_dev.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+Debug::enable();
+
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/dev.php';
+require __DIR__.'/../src/controllers.php';
+$app->run();
