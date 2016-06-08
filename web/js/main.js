6862e66719b12cfb6d80c73d66c7ea53d536b156
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2016-06-08 20:14:58 +0000

    added block to layout twig to set classes on the the html tag

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     $('#deletePrompt').on('show.bs.modal', function (event) {
       var $button = $(event.relatedTarget);
       var $modal = $(this);
