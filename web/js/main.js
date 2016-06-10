97380e9b4d54c3b4a483ca4e4cc28bdebcc41800
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2016-06-10 00:14:24 +0000

    hacked in some css for the history view

--- web/js/main.js
+++ web/js/main.js
@@ -9,4 +9,31 @@ $(document).ready(function() {
       $modalForm.find('.revision-val').val($activeForm.find('.revision-val').val());
       $modalForm.find('.workflow-state-val').val($activeForm.find('.workflow-state-val').val());
     });
+
+    $('.json-highlight').each(function(idx, el) {
+        console.log("yay!");
+        $(el).html(JSONHighlight($(el).text()));
+    });
 });
+
+function JSONHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+};
