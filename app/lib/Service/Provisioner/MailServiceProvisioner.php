6798a7449e361669ca41d7e94ba734275c52a446
Hasham Ahmad <mr.hash@gmail.com>
2016-07-26 22:19:50 +0000

    Mail service provisioning

---
+++ app/lib/Service/Provisioner/MailServiceProvisioner.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Honeybee\FrameworkBinding\Silex\Service\Provisioner;
+
+use Auryn\Injector;
+use Honeybee\Infrastructure\Config\ArrayConfig;
+use Honeybee\Infrastructure\Config\SettingsInterface;
+use Honeybee\Infrastructure\Mail\MailServiceInterface;
+use Honeybee\FrameworkBinding\Silex\Config\ConfigProviderInterface;
+use Honeybee\ServiceDefinitionInterface;
+use Pimple\Container;
+use Psr\Log\LoggerInterface;
+use Silex\Provider\SwiftmailerServiceProvider;
+
+class MailServiceProvisioner implements ProvisionerInterface
+{
+    const CONFIG_NAME = 'mail.yml';
+
+    public function provision(
+        Container $app,
+        Injector $injector,
+        ConfigProviderInterface $configProvider,
+        ServiceDefinitionInterface $serviceDefinition,
+        SettingsInterface $provisionerSettings
+    ) {
+        $service = $serviceDefinition->getClass();
+        $config = $configProvider->provide(self::CONFIG_NAME);
+
+        $app->register(new SwiftmailerServiceProvider);
+
+        if (isset($config['swiftmailer']['options'])) {
+            $app['swiftmailer.options'] = $config['swiftmailer']['options'];
+        }
+
+        if (isset($config['swiftmailer']['use_spool'])) {
+            $app['swiftmailer.use_spool'] = $config['swiftmailer']['use_spool'];
+        }
+
+        return $injector
+            ->share($service)
+            ->alias(MailServiceInterface::CLASS, $service)
+            ->delegate(
+                $service,
+                function (LoggerInterface $logger) use ($service, $app) {
+                    return new $service($app['mailer'], new ArrayConfig([]), $logger);
+                }
+            );
+    }
+}
