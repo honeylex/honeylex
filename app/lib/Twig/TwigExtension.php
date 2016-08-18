a6b7f6dcf3997729a1de47861142df4cddd19c33
Hasham Ahmad <mr.hash@gmail.com>
2016-08-19 01:09:53 +0200

    Fix twig extension

--- app/lib/Twig/TwigExtension.php
+++ app/lib/Twig/TwigExtension.php
@@ -7,6 +7,7 @@ use Honeybee\Projection\ProjectionInterface;
 use Honeybee\Projection\WorkflowSubject;
 use Twig_Extension;
 use Twig_SimpleFilter;
+use Workflux\Error\Error;
 
 class TwigExtension extends Twig_Extension
 {
@@ -26,8 +27,11 @@ class TwigExtension extends Twig_Extension
             }),
             new Twig_SimpleFilter('accepts_event', function (ProjectionInterface $entity, $event) {
                 $stateMachine = $entity->getType()->getWorkflowStateMachine();
-                $acceptedEvents = WorkflowSubject::getSupportedEventsFor($stateMachine, $entity->getWorkflowState());
-
+                try {
+                    $acceptedEvents = WorkflowSubject::getSupportedEventsFor($stateMachine, $entity->getWorkflowState());
+                } catch (Error $error) {
+                    return false;
+                }
                 return in_array($event, $acceptedEvents);
             })
         ];
