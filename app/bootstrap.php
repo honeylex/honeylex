31cd254bc735244b991f0a2fc5f2021d3598c96a
Hasham Ahmad <mr.hash@gmail.com>
2017-03-31 16:47:01 +0100

    Replace version file with env var

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,11 +5,11 @@ use Silex\Application;
 $projectConfigDir = __DIR__.'/config';
 $application = new Application;
 $configuration = [
-    'version' => trim(file_get_contents(dirname(__DIR__).'/VERSION.txt')),
-    'hostPrefix' => $hostPrefix,
+    'appVersion' => $appVersion,
     'appContext' => $appContext,
     'appEnv' => $appEnv,
     'appDebug' => filter_var($appDebug, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE),
+    'hostPrefix' => $hostPrefix,
     'core' => [
         'config_dir' => __DIR__.'/config/default',
         'dir' => dirname(__DIR__)
